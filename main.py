3c09b137eeb54dfb0379cf9baa967238e31ebc49
3xploit1 <novitsckij.gleb@yandex.ru>
2022-03-23 19:18:30 +0300

    add_take_kivy_client

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
+from kivy.clock import mainthread
 
 import paho.mqtt.client as mqtt
 import threading
@@ -15,6 +16,9 @@ Window.clearcolor = (.9, .9, .9, 1)
 
 
 class Container(GridLayout):
+    def __init__(self):
+        super(Container,self).__init__()
+        self.take_message()
 
     def start_send(self):
         threading.Thread(target=self.public,daemon=True).start()
@@ -44,12 +48,13 @@ class Container(GridLayout):
         
         client.publish(pubtop,text)
     
-    def start_take(self):
-        threading.Thread(target=self.take_message,daemon=True).start()
+    # def start_take(self):
+    #     threading.Thread(target=self.take_message,daemon=True).start()
     
+    @mainthread    
     def on_message(self,client,userdata,message):
         if message.topic == 'mqtt/example1':
-            self.ids.label_out.text(message.topic + ' ' + message.payload.decode('utf-8') + '\n')
+            self.ids.label_out.text = message.topic + '  ' + message.payload.decode('utf-8')
     
     def take_message(self):
         broker = "test.mosquitto.org"
@@ -60,8 +65,10 @@ class Container(GridLayout):
         client.subscribe('mqtt/example1')
         client.loop_start()
               
-        while True:
-            client.on_message = self.on_message
+        # while True:
+        client.on_message = self.on_message
+        # ---> бесконечный цикл вызывает огромное потребление ресурсов цп 
+        
 
 
 class MyApp (App):
