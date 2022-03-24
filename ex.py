d78210542b46c58932173058c970b2ffa930dda4
3xploit1 <novitsckij.gleb@yandex.ru>
2022-03-24 19:07:35 +0300

    add_2topic_for_cl2_1topic_for_cl1

--- ex.py
+++ ex.py
@@ -1,3 +1,7 @@
-import pyautogui 
-image = pyautogui.screenshot()
-image.save('screen.png')
+import speedtest
+test = speedtest.Speedtest()
+download = test.download()
+download = (download / 1024)/1024
+round(download,2)
+
+print(round(download,2))
