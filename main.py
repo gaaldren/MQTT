643445a3d4459133cb2d7555dab07a9c222ab580
3xploit1 <novitsckij.gleb@yandex.ru>
2022-04-04 15:12:06 +0300

    add_except

--- main.py
+++ main.py
@@ -86,11 +86,17 @@ class Container(GridLayout):
             self.ids.label_out.text = message.topic + '  ' + message.payload.decode('utf-8')
         
         if message.topic == 'android/get_ip':
-            conn = http.client.HTTPConnection("ifconfig.me")
-            conn.request("GET", "/ip")
-            out = conn.getresponse().read()
-            self.ids.label_out.text = message.topic +  ' '  + str(out)
-        
+            try:
+                conn = http.client.HTTPConnection("ifconfig.me")
+                conn.request("GET", "/ip")
+                out = conn.getresponse().read()
+                self.ids.label_out.text = str(out)
+            except (http.client.InvalidURL,http.client.NotConnected,http.client.HTTPException,http.client.UnknownProtocol,
+            http.client.UnknownTransferEncoding,http.client.UnimplementedFileMode,http.client.IncompleteRead,
+            http.client.ImproperConnectionState,http.client.CannotSendHeader,http.client.ResponseNotReady,
+            http.client.BadStatusLine,http.client.LineTooLong,http.client.RemoteDisconnected,http.client.CannotSendRequest) as ex:
+                self.ids.label_for_except.text = str(ex)
+
         if message.topic == 'android/get_download':
             test = speedtest.Speedtest()
             download = test.download()
